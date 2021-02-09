7b367d539fd8d6780bfb0b56a61e79814de01be2
NicatMirzoev <nicatmirzoev111@gmail.com>
2021-02-09 13:30:48 +0400

    added homepage

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile('index.html')
+})
+
+app.get('/chat', (req, res) => {
+  console.log(req.query);
+  res.send('test');
 })
 
 app.listen(port, () => {
