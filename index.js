08b6ddee366ac44ce47a4aedff57db07fd14015d
NicatMirzoev <nicatmirzoev111@gmail.com>
2021-02-11 17:26:49 +0400

    implemented messaging, leaving joining etc.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = 3000
-const users = {};
+let users = [];
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
@@ -13,25 +13,45 @@ app.get('/', (req, res) => {
 
 io.on('connection', socket => {
    console.log('A user connected');
-   socket.on('disconnect', function () {
-      console.log('A user disconnected');
-   });
-
    socket.on("join-room", (data) => {
      const { username, room } = data;
      if(username === undefined || room === undefined) {
        socket.emit("join-room", {error: "Please enter name and room name."})
      }
      else {
-       if(users[username] === undefined) {
-         users[username] = {room: room, typing: false}
-         socket.emit("join-room", {error: ""})
+       const found = users.findIndex(i => i[username] !== undefined);
+       if(found == -1) {
+         const user = {
+           [username]: {room: room, typing: false, id: socket.id}
+         }
+         users.push(user);
+         io.to(room).emit('user-joined', user)
+         socket.join(room);
+         socket.emit("join-room", {error: "", users: users.filter(user => user[Object.keys(user)[0]].room === room)})
          console.log(`User ${username} has joined to room ${room}`)
        }
        else {
          socket.emit("join-room", {error: "This username already taken, please choose another name."})
        }
      }
+   })
+   socket.on("chat-message", data => {
+     io.to(data.room).emit("chat-message", {username: data.username, message: data.message})
+   })
+   socket.on("leave-room", username => {
+     const user = users.find(user => Object.keys(user)[0] === username)
+     if(user !== undefined) {
+       io.to(user[Object.keys(user)[0]].room).emit('user-left', user)
+       users = users.filter(i => i !== user);
+     }
+   })
+   socket.on("disconnect", () => {
+     const user = users.find(user => user[Object.keys(user)[0]].id === socket.id)
+     if(user !== undefined) {
+       io.to(user[Object.keys(user)[0]].room).emit('user-left', user)
+       users = users.filter(i => i !== user);
+     }
+     console.log('A user disconnected');
    })
 });
 
